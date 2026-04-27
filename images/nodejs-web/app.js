777699502e6bc6253fe44896aafbedf1d01f63ee
Hendri <info@quezio.com>
2026-04-27 09:20:15 +0700

    k8s objects

---
+++ images/nodejs-web/app.js
@@ -0,0 +1,14 @@
+import { createServer } from 'http';
+
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const server = createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Application 2.0');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running`);
+});
